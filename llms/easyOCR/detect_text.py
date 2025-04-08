2c91885185e0e1a642bc81c0d45915294181d7fb
shadmau <maduakorfelix@gmail.com>
2025-04-08 18:49:49 +0800

    chore(core): Refactor project structure and remove experiments

--- llms/easyOCR/detect_text.py
+++
@@ -1,7 +0,0 @@
-import sys
-import easyocr
-reader = easyocr.Reader(['en'])
-
-image_path = sys.argv[1]
-text = reader.readtext(image_path, detail=0)
-print("\n".join(text))
--- poc-captcha/README.md
+++
@@ -1,3 +0,0 @@
-# reCAPTCHA v2 POC
-
-This is a proof-of-concept (POC) for Google's reCAPTCHA v2 implementation, intended to demonstrate integration and testing approaches.
